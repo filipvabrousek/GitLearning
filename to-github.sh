14e1433776882719a084fa37afcded99789c5462
Filip Vabrousek <filipvabrousek1@gmail.com>
2022-02-04 23:03:32 +0100

    Create to-github.sh

---
+++ to-github.sh
@@ -0,0 +1,10 @@
+Create a new Repository secret on Web
+git init // inside new folder
+git remote set-url origin https://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@github.com/RunnyApp/Secret
+
+
+// inside folder
+touch bond.txt
+git add bond.txt
+git commit -m “Add Bond”
+git push origin

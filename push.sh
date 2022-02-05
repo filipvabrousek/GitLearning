cd494be7275b06a2c9e23eb0b156a263b4d0796f
Filip Vabrousek <filipvabrousek1@gmail.com>
2022-02-05 11:49:50 +0100

    Create push.sh

---
+++ push.sh
@@ -0,0 +1,9 @@
+mkdir te
+cd te
+git init
+git remote add origin https://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@github.com/RunnyApp/BREP
+touch a.txt
+git add a.txt
+git commit -m "first commit"
+git branch -M main
+git push -u origin main
